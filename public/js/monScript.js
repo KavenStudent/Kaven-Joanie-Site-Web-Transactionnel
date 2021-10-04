2e065daa3fd9a16204e8bdb33e41f23150782fa0
joanieBirtz2000 <56734772+joanieBirtz2000@users.noreply.github.com>
2021-10-04 14:10:05 -0400

    toast
    asd

--- public/js/monScript.js
+++ public/js/monScript.js
@@ -71,9 +71,10 @@ let initialiser = (message) =>{
   })
   if(message.length > 0){
     textToast.innerHTML = message;
+    $(".toast-container").css("display", "block");
     toastList[0].show();
   }
 }
-// $(document).ready(function(){
-//   listerFilms();
-// });
+ $(document).ready(function(){
+  $(".toast-container").css("display", "none");
+ });
